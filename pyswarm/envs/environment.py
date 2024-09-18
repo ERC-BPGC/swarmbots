15363a2b452e578fe81e2d3d2c2a5052656761c6
akagam1 <arjunputhli00@gmail.com>
2024-09-18 16:14:26 +0530

    environment base class

---
+++ pyswarm/envs/environment.py
@@ -0,0 +1,10 @@
+from base import BaseEnvironment
+class Environment(BaseEnvironment):
+    """
+    Environment wrapper class
+    """
+
+    def __init__(self):
+        super().__init__()
+    
+       
